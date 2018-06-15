a3a61c6e2e816080fab75c6e5e772f1862c137ef
Steve Wagner <ciroque@live.com>
2018-06-15 14:35:13 -0700

    Fixes Call response

--- lib/messaging_status_service_web/controllers/api/call_controller.ex
+++ lib/messaging_status_service_web/controllers/api/call_controller.ex
@@ -8,10 +8,9 @@ defmodule MessagingStatusServiceWeb.Api.CallController do
   def create(conn, params) do
     Logger.debug("#{__MODULE__} create #{inspect(params)}")
     @call_completed_handler.handle_call_completed(params)
-    conn |> put_status(:accepted) |> render("create.json", %{})
-  end
-
-  def index(conn, _params) do
-    conn |> render("index.json", %{})
+    conn
+    |> put_status(:created)
+    |> put_resp_content_type("text/xml")
+    |> render("hang-up.xml", %{})
   end
 end
