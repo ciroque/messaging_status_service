5a5c636bda2ac9d9856ce8eb3412048d7d83eb89
Steve Wagner <ciroque@live.com>
2018-06-15 09:03:07 -0700

    Adds migrations for SMS logs

--- priv/repo/migrations/20180609021337_create_sms_status_table.exs
+++ priv/repo/migrations/20180609021337_create_sms_status_table.exs
@@ -1,15 +1,26 @@
-defmodule MessagingStatusService.Repo.Migrations.CreateSmsStatusTable do
+defmodule MessagingStatusService.Repo.Migrations.CreateSmsLogsTable do
   use Ecto.Migration
 
   def change do
-    create table(:sms_statuses) do
-      add(:to, :string)
+    create table(:sms_logs) do
+      add(:account_sid, :string)
+      add(:body, :string)
+      add(:date_created, :utc_datetime)
+      add(:date_sent, :utc_datetime)
+      add(:date_updated, :utc_datetime)
+      add(:direction, :string)
+      add(:error_code, :string)
+      add(:error_message, :string)
       add(:from, :string)
-      add(:message_sid, :string)
-      add(:message_status, :string)
-      add(:sms_sid, :string)
-      add(:sms_status, :string)
-      add(:api_version, :string)
+      add(:messaging_service_sid, :string)
+      add(:num_media, :string)
+      add(:num_segments, :string)
+      add(:price, :string)
+      add(:price_unit, :string)
+      add(:sid, :string)
+      add(:status, :string)
+      add(:to, :string)
+      add(:uri, :string)
 
       timestamps()
     end
