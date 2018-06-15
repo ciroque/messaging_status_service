86f1cf081e6fb1802ecac50212ab823ca7519926
Steve Wagner <ciroque@live.com>
2018-06-15 09:24:36 -0700

    Adds schema and Repo helpers

---
+++ lib/messaging_status_service/sms_sids/sms_sid.ex
@@ -0,0 +1,24 @@
+defmodule MessagingStatusService.SmsSid do
+  use Ecto.Schema
+  import Ecto.Changeset
+  import Honeydew.EctoPollQueue.Schema
+
+  @timestamps_opts [usec: false]
+
+  schema "sms_sids" do
+    field(:sms_sid, :string)
+    honeydew_fields(:sms_sid_handler)
+    timestamps()
+  end
+
+  def changeset(attrs) do
+    %__MODULE__{}
+    |> changeset(attrs)
+  end
+
+  def changeset(%__MODULE__{} = sms_sid, attrs) do
+    sms_sid
+    |> cast(attrs, [:sms_sid])
+    |> validate_required([:sms_sid])
+  end
+end
